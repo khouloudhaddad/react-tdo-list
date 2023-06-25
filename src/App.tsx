d68c2d0391f1862c7e075e4a67633708320d28b7
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-06-25 10:25:40 +0000

    Persist todos in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 import  NewTodoForm  from "./components/NewTodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
   
-  const [todos, setTodos] = useState([]); 
+  const [todos, setTodos] = useState(() => {
+    const currentTodos = localStorage.getItem("ITEMS");
+    if (currentTodos == null) return []
+    
+    return JSON.parse(currentTodos);
+  }); 
+
+  //save todos in local storage everytime todos list changes
+  useEffect(() => {
+    localStorage.setItem('ITEMS', JSON.stringify(todos))
+  },[todos])
 
   function addTodo(title) {
     setTodos((currentTodos) => {
