eb29313e7561fb422c14b6adcdad8f6d83e8ee13
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-06-25 09:29:16 +0100

    Add label

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import './styles.css'
 
 function App() {
 
-  return 
+  return (
     <form className='new-item-form'>
       <div className='form-row'>
-        <label htmlFor='item'></label> 
+        <label htmlFor='item'>New Item</label> 
         <input type='text' id='item' />
       </div>
     </form>
-  
+  )
 }
 
 export default App
