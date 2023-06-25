59edcc6dc6cbb7401f9a468f4bab1abf659e64c9
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-06-25 09:57:48 +0100

    Display todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { useState } from 'react';
-import './styles.css'
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
-
   const [newItem, setNewItem] = useState("");
   const [todos, setTodos] = useState([]);
 
   function handleSubmit(e) {
-    e.preventDefault()
-    setTodos([
-      ...todos,
-      {
-        id: crypto.randomUUID(),
-        title: newItem,
-        completed: false
-      },
-    ])
+    e.preventDefault();
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false,
+        },
+      ];
+    });
   }
-
+  console.log(todos);
   return (
     <>
-      <form onSubmit={handleSubmit}
-        className="new-item-form"
-      >
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input value={newItem}
-            onChange={e => setNewItem(e.target.value)}
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
             type="text"
             id="item"
           />
@@ -35,23 +35,20 @@ function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map(todo => {
+          return (
+            <li>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          )
+        })}
       </ul>
     </>
   );
 }
 
-export default App
+export default App;
